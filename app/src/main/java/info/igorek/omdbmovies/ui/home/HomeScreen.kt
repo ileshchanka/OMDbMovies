3eef10d2178fc8ad17d5585b571fd309afc13889
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2023-06-16 14:55:14 +0200

    FAT-58231: Init HomeScreen

--- app/src/main/java/info/igorek/omdbmovies/ui/home/HomeScreen.kt
+++ app/src/main/java/info/igorek/omdbmovies/ui/home/HomeScreen.kt
@@ -1,29 +1,81 @@
 package info.igorek.omdbmovies.ui.home
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeScreenViewModel,
 ) {
-//    val state by viewModel.state.collectAsState()
+    val state by viewModel.state.collectAsState()
+    val text = remember { mutableStateOf("") }
 
-    Text(text = "HOME SCREEN")
-
-    TextField(value = "Field", onValueChange = {})
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(8.dp),
     ) {
-//        RateTable(state.rateList)
+        item {
+            OutlinedTextField(
+                value = text.value,
+                onValueChange = {
+                    text.value = it
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                singleLine = true,
+            )
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+            ) {
+                Text(
+                    text = "Find",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+        items(state.movieList) { movie ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                GlideImage(
+                    model = movie.poster,
+                    contentDescription = "${movie.title} Poster",
+                )
+                Text(
+                    text = movie.title,
+                    modifier = Modifier.padding(start = 8.dp),
+                )
+
+            }
+        }
     }
 }
--- app/src/main/java/info/igorek/omdbmovies/ui/home/HomeScreenViewModel.kt
+++ app/src/main/java/info/igorek/omdbmovies/ui/home/HomeScreenViewModel.kt
@@ -2,6 +2,7 @@ package info.igorek.omdbmovies.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import info.igorek.omdbmovies.api.model.ui.SearchResultUi.MovieResultUi
 import info.igorek.omdbmovies.api.repository.MoviesRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,29 +14,22 @@ class HomeScreenViewModel(
     private val moviesRepository: MoviesRepository,
 ) : ViewModel() {
 
-//    data class State(
-//        val table: String = EMPTY_STRING,
-//        val no: String = EMPTY_STRING,
-//        val effectiveDate: String = EMPTY_STRING,
-//        val rateList: List<RateUi> = emptyList(),
-//    )
+    data class State(
+        val movieList: List<MovieResultUi> = emptyList(),
+    )
 
-//    private val _state = MutableStateFlow(State())
-//    val state: StateFlow<State> = _state
+    private val _state = MutableStateFlow(State())
+    val state: StateFlow<State> = _state
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
             val item = moviesRepository.search("tron")
-            item
 
-//            _state.update {
-//                it.copy(
-//                    table = item.table,
-//                    no = item.no,
-//                    effectiveDate = item.effectiveDate,
-//                    rateList = item.rates,
-//                )
-//            }
+            _state.update {
+                it.copy(
+                    movieList = item.movieResults,
+                )
+            }
         }
     }
 }
