0084fa9005b4d255ee78f7f0c898a2e7f5e6de72
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2023-06-21 11:18:03 +0200

    FAT-58231: DetailsScreen modification

--- app/src/main/java/info/igorek/omdbmovies/ui/details/DetailsScreen.kt
+++ app/src/main/java/info/igorek/omdbmovies/ui/details/DetailsScreen.kt
@@ -1,9 +1,22 @@
 package info.igorek.omdbmovies.ui.details
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 
@@ -14,10 +27,34 @@ fun DetailsScreen(
 ) {
     val state by viewModel.state.collectAsState()
 
-    GlideImage(
-        model = state.poster,
-        contentDescription = "${state.title} Poster"
-    )
-    Text(text = state.title)
-    Text(text = state.plot)
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(8.dp),
+    ) {
+        item {
+            GlideImage(
+                model = state.poster,
+                contentDescription = "${state.title} Poster",
+                modifier = Modifier.height(300.dp),
+            )
+            Text(
+                text = state.title, fontSize = 24.sp, modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+
+        items(state.ratingList) { rating ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(text = rating.source)
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = rating.value)
+            }
+        }
+
+        item {
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            Text(text = state.plot)
+        }
+    }
 }
